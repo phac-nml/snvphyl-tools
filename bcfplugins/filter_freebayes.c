4362ebbc32d14e61d04a5b0b9356b06178f51d86
phil <philip.mabon@phac-aspc.gc.ca>
2015-02-16 12:02:04 -0600

    re-implemented plugins to work with versioned copies of bcftools. Also commented out filtering stages so the results from plugins is very similar to aaron version of the pipeline. Will have to turn them into options later

--- bcfplugins/filter_freebayes.c
+++ bcfplugins/filter_freebayes.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <stdarg.h>
+#include <stdint.h>
 #include <htslib/vcf.h>
-#include "config.h"
-#include <wordexp.h>
+#include <htslib/regidx.h>
+#include <htslib/synced_bcf_reader.h>
+#include <htslib/vcfutils.h>
+#include "bcftools.h"
 
 /* 
     This short description is used to generate the output of `bcftools annotate -l`.
@@ -14,6 +19,13 @@ const char *about(void)
         "Percentage of alternative alleles from total DP of coverage (INFO/AO / INFO/DP ) * 100 \n";
 }
 
+const char *usage(void)
+{
+  return "";
+
+}
+
+
 int dp_type = BCF_HT_INT;
 int mqm_type = BCF_HT_REAL;
 int type_type = BCF_HT_STR;
@@ -23,40 +35,49 @@ float min_mqm = -1;
 bcf_hdr_t *in_hdr = NULL;
 bcf_hdr_t *out_hdr = NULL;
 int flag_coverage=-1;
+int verbose =-1;
 
 /* 
     Called once at startup, allows to initialize local variables.
     Return 1 to suppress VCF/BCF header from printing, 0 otherwise.
 */
-int init(const char *opts, bcf_hdr_t *in, bcf_hdr_t *out)
+int init(int argc, char **argv, bcf_hdr_t *in, bcf_hdr_t *out)
 {
 
-  wordexp_t wexp;
-  
-  char *temp = config_get_string(opts,"dp");
 
+    static struct option loptions[] =
+    {
+        {"verbose",1,0,'v'},
+        {"dp",1,0,'d'},
+        {"mqm",1,0,'m'},
+        {"ao",1,0,'a'},
+        {0,0,0,0}
+    };
+    char c, *tmp;
+    while ((c = getopt_long(argc, argv, "d:v",loptions,NULL)) >= 0)
+    {
+        switch (c) {
+            case 'v': verbose = 1; break; 
+            case 'd': 
+                min_dp = strtol(optarg,&tmp,10); 
+                if (*tmp) error("Unexpected argument to --dp: %s\n", optarg); break; 
+            case 'm': 
+                min_mqm = strtol(optarg,&tmp,10); 
+                if (*tmp) error("Unexpected argument to --mqm: %s\n", optarg); break; 
+            case 'a': 
+                min_ao = strtol(optarg,&tmp,10); 
+                if (*tmp) error("Unexpected argument to --ao: %s\n", optarg); break; 
+            case 'h':
+            case '?':
+            default: error("%s", usage()); break;
+        }
+    }
 
-  wordexp(temp, &wexp, 0);
-  min_dp = atoi(wexp.we_wordv[0]);
-
-  temp = config_get_string(opts,"mqm");
-  wordexp(temp, &wexp, 0);
-
-
-  min_mqm = atoi(wexp.we_wordv[0]);
-  temp = config_get_string(opts,"ao");
-
-
-  wordexp(temp, &wexp, 0);
-  min_ao = atoi(wexp.we_wordv[0]);
-  
-
-  free(temp);
   
   in_hdr  = in;
   out_hdr  = out;
 
-  char info[80];
+  char info[100];
   sprintf(info, "##FILTER=<ID=filtered-coverage,Description=\"Set true if DP < %d\">", min_dp);
   bcf_hdr_append(out_hdr, info);
     
@@ -74,26 +95,30 @@ int init(const char *opts, bcf_hdr_t *in, bcf_hdr_t *out)
     Called for each VCF record after all standard annotation things are finished.
     Return 0 on success, 1 to suppress the line from printing, -1 on critical errors.
 */
-int process(bcf1_t *rec)
+bcf1_t *process(bcf1_t *rec)
 {
 
   int *buf = NULL;
   int nbuf = 0;   // NB: number of elements, not bytes
   int DP =-1;
+  int dp_set=0;
   if (bcf_get_info_values(in_hdr,rec,"DP",(void**)&buf,&nbuf,dp_type)) {
     DP = *buf;
 
     //check to see if we have or over the minimum depth of coverage
     //if not we are softclipping out of file
     if (DP < min_dp) {
-      bcf_add_filter(out_hdr,rec,flag_coverage);
+//take out for now otherwise not same answer as aaron
+////had to put back all the filtering directly when running the pipeline
+    //  bcf_add_filter(out_hdr,rec,flag_coverage);
+    //  dp_set=1;
     }
   }
-  else {
+  /*  else {
     printf ("A record does not contain DP flag in INFO column\n");
     return -1;
   }
-
+  */
 
   float *buf2 = NULL;
   int nbuf2 = 0;   // NB: number of elements, not bytes
@@ -103,14 +128,14 @@ int process(bcf1_t *rec)
     //check to see if we have good mean mapping quality of observed for alternate alleles
     //if not we are hardclipping out of file
     if (MQM < min_mqm) {
-      return 1;
+      return NULL;
     }
   }
-  else {
+  /*  else {
     printf ("A record does not contain MQM flag in INFO column\n");
     return -1;
   }
-
+  */
 
   //get INFO/AO and see if over % of total reads
 
@@ -120,14 +145,14 @@ int process(bcf1_t *rec)
     //determine percentage of reads that match the alternative allele
     double test = (double)AO/DP * 100;
     if (test < min_ao ) {
-      return 1; 
+      return NULL; 
     }
   }
-  else {
+  /*  else {
     printf ("A record does not contain AO flag in INFO column\n");
     return -1;
   }
-
+  */
 
 
 /* turn off complex/mnp region filtering completely for now. 
@@ -139,17 +164,22 @@ int process(bcf1_t *rec)
 
     //we are throwing out complex or mnp regions
     if (strcmp(type,"complex") == 0 || strcmp(type,"mnp") == 0) {
-      return 1;
+      return NULL;
     }
   }
+  
   else {
     printf ("A record does not contain TYPE flag in INFO column\n");
     return -1;
   }
 */
   //add that we passed all filtering!
-  bcf_add_filter(out_hdr,rec,0);
-  return 0;
+  //only add pass flag if 'filtered-coverage' not set
+  if (! dp_set){
+    bcf_add_filter(out_hdr,rec,0);
+  }
+
+  return rec;
 }
 
 
--- bcfplugins/filter_mpileup.c
+++ bcfplugins/filter_mpileup.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <stdarg.h>
+#include <stdint.h>
 #include <htslib/vcf.h>
-#include "config.h"
-#include <wordexp.h>
+#include <htslib/regidx.h>
+#include <htslib/synced_bcf_reader.h>
+#include <htslib/vcfutils.h>
+#include "bcftools.h"
 
 /* 
     This short description is used to generate the output of `bcftools annotate -l`.
@@ -10,48 +15,83 @@
 const char *about(void)
 {
     return 
-        "A minimal plugin which counts number of SNPs, Indels, and\n"
-        "total number of sites.\n";
+        "A plugin to soft filter position if they have a SNP or have low coverage.\n";
 }
 
-int min_dp =-1;
-bcf_hdr_t *in_hdr = NULL;
-bcf_hdr_t *out_hdr = NULL;
-int flag_coverage=-1;
-int flag_mpileup=-1;
+const char *usage(void)
+{
+  return "";
+
+}
+
+
+int min_dp;
+bcf_hdr_t *in_hdr;
+bcf_hdr_t *out_hdr;
+int flag_coverage;
+int flag_mpileup;
+int verbose;
+
 
 /* 
     Called once at startup, allows to initialize local variables.
     Return 1 to suppress VCF/BCF header from printing, 0 otherwise.
 */
-int init(const char *opts, bcf_hdr_t *in, bcf_hdr_t *out)
+int init(int argc, char **argv, bcf_hdr_t *in, bcf_hdr_t *out)
+
 {
-  char *temp = config_get_string(opts,"dp");
+ 
+    min_dp=-1;
+    flag_coverage=-1;
+    flag_mpileup=-1;
+    verbose=-1;
 
-  wordexp_t wexp;
-  wordexp(temp, &wexp, 0);
+    static struct option loptions[] =
+    {
+        {"verbose",1,0,'v'},
+        {"dp",1,0,'d'},
+        {0,0,0,0}
+    };
+    char c, *tmp;
+    while ((c = getopt_long(argc, argv, "d:v",loptions,NULL)) >= 0)
+    {
+        switch (c) {
+            case 'v': verbose = 1; break; 
+            case 'd': 
+                min_dp = strtol(optarg,&tmp,10); 
+                if (*tmp) error("Unexpected argument to --dp: %s\n", optarg); break; 
+            case 'h':
+            case '?':
+            default: error("%s", usage()); break;
+        }
+    }
 
-  free(temp);
-  min_dp = atoi(wexp.we_wordv[0]);
-  printf("Received minimum coverage of '%d'\n",min_dp);
+    in_hdr  = in;
+    out_hdr  = out;
+
+
+    //add this bogus flag otherwise for some reason the 'DP4' flag is the same id as first filtered 
+    bcf_hdr_append(out_hdr,"##FILTER=<ID=DP44,Number=4,Type=Integer,Description=\"Number of high-quality ref-forward , ref-reverse, alt-forward and alt-reverse bases\">");
+    int dp4 = bcf_hdr_id2int(out_hdr, BCF_DT_ID, "DP44");
+    dp4=-1;
+    //needs to golf down into a simple plugin and reported as a bug. Phil Feb 16,2015 
+
+
+    char info[100];
+    sprintf(info, "##FILTER=<ID=filtered-coverage,Description=\"Set true if DP is less then %d\">", min_dp);
+    bcf_hdr_append(out_hdr, info);
+
+    //get flag index id so we can mark as such
+    flag_coverage = bcf_hdr_id2int(out_hdr,BCF_DT_ID,"filtered-coverage");
+
   
-  in_hdr  = in;
-  out_hdr  = out;
+          
+    bcf_hdr_append(out_hdr, "##FILTER=<ID=filtered-mpileup,Description=\"Set if not true: SNP not found in Freebayes\">");
+    
+    //get flag index id so we can mark as such
+    flag_mpileup = bcf_hdr_id2int(out_hdr, BCF_DT_ID, "filtered-mpileup");
 
-  char info[80];
-  sprintf(info, "##FILTER=<ID=filtered-coverage,Description=\"Set true if DP < %d\">", min_dp);
-  bcf_hdr_append(out_hdr, info);
-
-  //get flag index id so we can mark as such
-  flag_coverage = bcf_hdr_id2int(out_hdr,BCF_DT_ID,"filtered-coverage");
-
-
-  bcf_hdr_append(out_hdr, "##FILTER=<ID=filtered-mpileup,Description=\"Set if not true: SNP not found in Freebayes\">");
-
-  //get flag index id so we can mark as such
-  flag_mpileup = bcf_hdr_id2int(out_hdr,BCF_DT_ID,"filtered-mpileup");
-  
-  return 0;
+    return 0;
 }
 
 
@@ -60,19 +100,16 @@ int pl_type = BCF_HT_INT;
 int *buf = NULL;
 int nbuf = 0;   // NB: number of elements, not bytes
 
-/*
-    Called for each VCF record after all standard annotation things are finished.
-    Return 0 on success, 1 to suppress the line from printing, -1 on critical errors.
-*/
-int process(bcf1_t *rec)
+
+bcf1_t *process(bcf1_t *rec)
 {
 
+      
   if (bcf_get_info_values(in_hdr,rec,"DP",(void**)&buf,&nbuf,pl_type)) {
     int DP = *buf;
 
     //check to see if we have or over the minimum depth of coverage
     if (DP >= min_dp) {
-      //printf("DP is '%d'\n",DP);
       //since we are over the coverage, mark as PASS
       bcf_add_filter(out_hdr,rec,0);
     }
@@ -82,15 +119,15 @@ int process(bcf1_t *rec)
     }
   }
 
-
+  
   int type = bcf_get_variant_types(rec);
   //if we have SNP at this point, implies not found in freebayes so should mark as filtered-mileup
   if ( type&VCF_SNP ){
     bcf_add_filter(out_hdr,rec,flag_mpileup); 
   }
   
-  
-  return 0;
+
+  return rec;
 }
 
 
