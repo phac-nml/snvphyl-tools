197d58b315b56916f3a5a5672a8d7bff5c731f6f
csieffert <cameron.sieffert@phac-aspc.gc.ca>
2015-10-27 14:51:54 -0500

    Wrapper script for snp density bcftools plugin.  Altered the plugin so that it prints the regions of density to a tsv output file.  Fixed a few bugs that caused errors with edge case filtering.

--- bcfplugins/bcftools-1.2/plugins/filter_snp_density.c
+++ bcfplugins/bcftools-1.2/plugins/filter_snp_density.c
@@ -14,6 +14,7 @@ const char *usage(void);
 int mark_density_snp(bcf1_t *bcf_current, bcf_hdr_t *header, int flag_density);
 int check_density(bcf1_t *current, bcf1_t *next, int density_threshold);
 bcf1_t *process(bcf1_t *rec);
+void print_density_region(int start, int end, int chromosome);
 void destroy(void);
 
 //define the script variables
@@ -21,14 +22,20 @@ char *filename = NULL;
 int density_threshold = 10;
 int flag_density = -1;
 int verbose = -1;
+int eof = 0;
 bcf_hdr_t *in_hdr = NULL;
 bcf_hdr_t *out_hdr = NULL;
 int in_density_region = 0;
-
+char *regionFilepath=NULL;
+FILE *regionsFile;
 //for look ahead functionality
 htsFile *htsAhead=NULL; 
 bcf_hdr_t *htsAheadHdr = NULL;
 bcf1_t *bcf_ahead = NULL;
+
+//track the density regions
+int density_start_position = 0;
+int density_end_position = 0;
 
 const char *about(void)
 {
@@ -40,18 +47,22 @@ int init(int argc, char **argv, bcf_hdr_t *in, bcf_hdr_t *out)
 {
    static struct option loptions[] =
    {
-       {"filename",1,0,'f'},	   
+       {"filename",1,0,'f'},
+       {"region_file",1,0,'r'},	   
        {"threshold",1,0,'t'},
        {0,0,0,0}
    };
    char c, *tmp;
-   while((c = getopt_long(argc, argv, "f:t", loptions, NULL)) >= 0)
+   while((c = getopt_long(argc, argv, "f:r:t", loptions, NULL)) >= 0)
    {
        switch (c)
       {  
          case 'f':
 	       filename = optarg; 
 	       break;
+         case 'r':
+               regionFilepath = optarg;
+               break;
          case 't':	 
            density_threshold = strtol(optarg, &tmp, 10);
            break;
@@ -90,11 +101,13 @@ int init(int argc, char **argv, bcf_hdr_t *in, bcf_hdr_t *out)
 }
 
 bcf1_t *process(bcf1_t *hts){
+
    if(bcf_read(htsAhead, htsAheadHdr, bcf_ahead) == -1){
-      printf("Unable to read look ahead bcf record for analysis OR we have reached the end of the bcf file.\n");    	   
+      printf("Unable to read look ahead bcf record for analysis OR we have reached the end of the bcf file.\n");
+      eof = 1;    	   
    }
-   
-   if(in_density_region || check_density(hts, bcf_ahead, density_threshold)){
+      
+   if(check_density(hts, bcf_ahead, density_threshold)){
        mark_density_snp(hts, out_hdr, flag_density);
    }
    //return the bcf record
@@ -110,7 +123,8 @@ int mark_density_snp(bcf1_t *bcf_current, bcf_hdr_t *header, int flag_density){
 		return 1;
 	}
 	else{
-		return 0;
+            printf("Unable to add density flag to vcf record.");       
+	    return 0;
 	}
 }
 
@@ -120,16 +134,47 @@ int mark_density_snp(bcf1_t *bcf_current, bcf_hdr_t *header, int flag_density){
  * @return int 1 if it is a high density position, 0 if not
  */
 int check_density(bcf1_t *current, bcf1_t *next, int density_threshold){
-	int current_position = current->pos;
-	int next_position = next->pos;
-
-    if(((next_position - current_position) <= density_threshold) && ((current_position - next_position) <= density_threshold) && (current_position != next_position)){
+    int current_position = current->pos;
+    int next_position = next->pos;
+    
+    //check the absolute difference between snv's to determine if it is within a dense region 
+    if(((next_position - current_position) <= density_threshold) && ((current_position - next_position) <= density_threshold) && (current->rid == next->rid) && (!eof)){
+        //track the start and end positions for the density regions appropriately:
+        //if not previously in a denisty region, mark the start of density region:
+        if(!in_density_region){
+           density_start_position = current_position;
+        }
     	in_density_region = 1;
-	    return 1;
+	return 1;
     }
 
-	in_density_region = 0;
-	return 0;
+    if(in_density_region){
+        density_end_position = current_position;
+        //print the newly discovered density region to a tab seperated file:
+        print_density_region(density_start_position, density_end_position, current->rid);
+        //if we have come to the end of a density region, we still must ensure the last snv
+        //in the region is marked as filtered-density.
+        in_density_region = 0;
+        return 1;
+    }
+    in_density_region = 0;
+    return 0;
+}
+
+/**
+* Prints the high density regions to the output regions file in a tab seperated format.
+* @start The start position of the high density region
+* @end The end position of the high density region
+* @chromosome The name of the chromosome containing the region
+*/
+void print_density_region(int start, int end, int chromosome){
+   //we need to add 1 position as the output appears to start counting from 0, but this
+   //will create an off by one error in the tab seperated regions file.
+   start = start +1;
+   end = end +1;
+   regionsFile = fopen(regionFilepath, "a");
+   fprintf(regionsFile, "%d\t%d\t%d\n", chromosome, start, end);
+   fclose(regionsFile);
 }
 
 const char *usage(void)
--- t/filter_snp_density.t
+++ t/filter_snp_density.t
@@ -9,18 +9,18 @@ use File::Compare;
 use File::Temp qw(tempdir);
 
 my $script_dir = $FindBin::Bin;
-
 my $density_dir = "$script_dir/snp_density";
+my $regions_output = "$density_dir/density_regions.tsv";
 my ($command);
 #=============================================================================
 #UNIT TESTS
 my $create_dir = tempdir(TEMPLATE => 'tempXXXXX', CLEANUP => 1) or die "Unable to create temporary file directory.";
-system("bcftools plugin filter_snp_density $density_dir/input/1.bcf -O b -- --filename $density_dir/input/1.bcf --threshold 10 > $create_dir/temp1.bcf 2>&1");
+system("bcftools plugin filter_snp_density $density_dir/input/1.bcf -O b -- --filename $density_dir/input/1.bcf --region_file $regions_output  --threshold 10 > $density_dir/temp1.bcf 2>&1");
 ok(-e $create_dir."/temp1.bcf", "Output bcf file is in the correct location.");
 
 
 #2 => verify that negative threshold values are set to the default threshold value
-system("bcftools plugin filter_snp_density $density_dir/input/1.bcf -O b -o $create_dir/temp2.bcf -- -f $density_dir/input/1.bcf --threshold 5 2>&1");
+system("bcftools plugin filter_snp_density $density_dir/input/1.bcf -O b -o $create_dir/temp2.bcf -- -f $density_dir/input/1.bcf --region_file $regions_output --threshold 5 2>&1");
 my $lines1 = `bcftools view $density_dir/input/1.bcf | wc -l 2>&1`;
 my $lines2 = `bcftools view $create_dir/temp2.bcf | wc -l 2>&1`;
 my $output = `bcftools view $create_dir/temp2.bcf`;
@@ -29,6 +29,10 @@ ok($output =~ 'filtered-density', "SNP's are correctly being filtered for densit
 ok($output =~ '##FILTER=<ID=filtered-density,Description="Set true if spacing is < 5 bp">', "Filter descripition is added to header.");
 
 #3 => verify that an absent threshold value is set to the default value
-ok(!system("bcftools plugin filter_snp_density $density_dir/input/1.bcf -O b -o $create_dir/temp3.bcf -- -f $density_dir/input/1.bcf 2>&1"), "A default threshold value is set when the parameter is absent");
+ok(!system("bcftools plugin filter_snp_density $density_dir/input/1.bcf -O b -o $create_dir/temp3.bcf -- -f $density_dir/input/1.bcf --region_file $regions_output 2>&1"), "A default threshold value is set when the parameter is absent");
+
+
+ok(system("perl $script_dir/../filter_snp_density.pl --output-file $density_dir/new_out.bcf --output-regions $density_dir/new_regions.tsv --threshold 10 --bcf bcf1=$density_dir/input/1.bcf 2>&1"));
+
 
 done_testing();
