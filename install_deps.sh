4eb528555738a3b77de6f5842bf9182ba580c132
takadonet <philipmabon@gmail.com>
2017-08-18 10:51:10 -0500

    moved all dependencies building into a single shell script. The script original purpose is for CI and developer testing. It MAY be used for bioconda but rather use other recipe first.

---
+++ install_deps.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+set -e
+
+toolPATH=''
+plugins=''
+
+#flag to silent all make and configures being ran.
+#set to empty string if you want to see everything in its glory
+silent="--silent"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+
+echo "Fetching and installing htslib...";
+
+#installing all dependencies for building bcftools and our plugin
+htslib='htslib-1.5'
+
+if [ ! -d "$htslib" ]; then
+
+    rm -rf $htslib
+fi
+
+
+curl -s https://codeload.github.com/samtools/htslib/tar.gz/1.5 > htslib.tar.gz
+tar -zxf htslib.tar.gz
+#clean up
+rm htslib.tar.gz
+
+cd $htslib
+autoreconf 
+./configure $silent
+make $silent 2> /dev/null
+
+export HTSLIB=`pwd`/htslib
+cd $DIR
+
+echo "Done!"
+
+echo "Fetching and install bcftools"
+
+bcftools='bcftools-1.5'
+
+if [ ! -d "$bcftools" ]; then
+
+    rm -rf $bcftools
+fi
+
+
+
+curl -s https://codeload.github.com/samtools/bcftools/tar.gz/1.5 > bcftools.tar.gz
+tar -zxf bcftools.tar.gz
+
+#clean up
+rm bcftools.tar.gz
+
+cp bcfplugins/filter_snv_density.c bcftools-1.5/plugins
+cd bcftools-1.5
+
+autoreconf
+./configure $silent 2> /dev/null
+make $silent 2> /dev/null
+
+#add bcftools to the path
+toolPATH=`pwd`
+
+plugins=`pwd`/plugins
+
+cd $DIR
+
+echo "Done!"
+
+echo "Fetching and installing Mummer"
+
+mummer='MUMmer3.23'
+
+if [ ! -d "$mummer" ]; then
+
+    rm -rf $mummer
+fi
+
+
+#fetching and putting on the path mummer for find-repeats.pl script
+curl -s -L https://sourceforge.net/projects/mummer/files/mummer/3.23/MUMmer3.23.tar.gz/download > mummer.tar.gz
+tar -zxf mummer.tar.gz
+rm mummer.tar.gz
+
+cd $mummer
+#completely silenting the output from make
+make $silent 2> /dev/null
+
+#add mummer to the path
+toolPATH=$toolPATH:`pwd`
+
+cd $DIR
+
+echo "Done!";
+echo "Please source following environement variables"
+echo "export PATH=$toolPATH:\$PATH"
+echo "export BCFTOOLS_PLUGINS=$plugins"
+
+export PATH=$toolPATH:$PATH
+export BCFTOOLS_PLUGINS=$plugins
